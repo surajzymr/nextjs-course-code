b2c69a287e597410c43f5195137ad9c01d668dc8
“surajzymr” <suraj.rathod@zymr.com>
2023-09-13 11:09:41 +0530

    sfsdf

---
+++ pages/api/feedback.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const descText = req.body.text;
+
+    const newDescription = {
+      id: new Date().toISOString(),
+      email: email,
+      text: descText,
+    };
+
+    // now to store the above information in the inbuilt json file
+    // 1) we create a path for the data to be connected
+    const filePath = path.join(__dirname, "../data/descriptions.json");
+    // 2) we want to read the above file
+    const fileData = fs.readFileSync(filePath);
+    // 3) we need to read the data now in the desired json format
+    data.push(newDescription);
+    // 4) now we nened ot write the above data
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    // 5) now we need to pass a status message stating that it got updated.
+    res.status(201).json({ message: "Success", feedback: newDescription });
+  } else {
+    res.status(200).json({ message: "This works" });
+  }
+}
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,47 @@
+import { useRef, useState } from "react";
+
 function HomePage() {
+  const [emailValue, setValue] = useState();
+  const [descvalue, setDescValue] = useState();
+  const emailValueRef = useRef();
+  const descValueRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredemail = emailValueRef.current.value;
+    const enteredDesc = descValueRef.current.value;
+    setValue(enteredemail);
+    setDescValue(enteredDesc);
+    console.log("enteredDesc....", enteredemail);
+    console.log("enteredDesc...", enteredDesc);
+
+    const reqBody = { email: enteredemail, text: enteredDesc };
+
+    fetch("api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());   
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitHandler}>
+        <p htmlFor="email">Email Address: </p>
+        <input type="email" id="email" ref={emailValueRef} />
+        <br />
+        <p htmlFor="text">Description: </p>
+        <input type="text" id="text" ref={descValueRef} />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
+      <div>
+        <p>Email : {emailValue}</p>
+        <p>Description: {descvalue}</p>
+      </div>
     </div>
   );
 }
