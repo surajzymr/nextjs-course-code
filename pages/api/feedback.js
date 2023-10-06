fa49596f23605e6aee4d80564d8e38b9274bf416
“surajzymr” <suraj.rathod@zymr.com>
2023-10-06 17:18:59 +0530

    asfsdfsd

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -7,8 +7,8 @@ export function buildFeedbackPath() {
 
 export function extractFeedback(filePath) {
   const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
-    return data;
+  const data = JSON.parse(fileData);
+  return data;
 }
 
 function handler(req, res) {
@@ -22,21 +22,21 @@ function handler(req, res) {
     };
     // now to store the above information in the inbuilt json file
     // 1) we create a path for the data to be connected
-    
+
     const filePath = buildFeedbackPath();
     // 2) we want to read the above file
-    const data = extractFeedback(filePath)
-      // 3) we need to read the data now in the desired json format
+    const data = extractFeedback(filePath);
+    // 3) we need to read the data now in the desired json format
     data.push(newDescription);
-        // 4) now we nened ot write the above data
+    // 4) now we nened ot write the above data
     fs.writeFileSync(filePath, JSON.stringify(data));
     // 5) now we need to pass a status message stating that it got updated.
     res.status(201).json({ message: "Success", feedback: newDescription });
   } else {
     const filePath = buildFeedbackPath();
-    const data = extractFeedback(filePath)
+    const data = extractFeedback(filePath);
 
-    res.status(200).json({ feedback: data  });
+    res.status(200).json({ feedback: data });
   }
 }
 
